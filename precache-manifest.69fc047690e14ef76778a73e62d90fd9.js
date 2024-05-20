05ce18a4f5c75429dacb7cc25c8e6e50f649c012
yk <2622811984@qq.com>
2024-05-20 15:59:51 +0800

    feat: 修改文件

---
+++ precache-manifest.69fc047690e14ef76778a73e62d90fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680f9a932995aa1c374f35a6069721fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "177989b692968e92964e",
+    "url": "./static/css/2.6e2b2ac5.chunk.css"
+  },
+  {
+    "revision": "b6d424695dc93b12ca54",
+    "url": "./static/css/main.607a57b5.chunk.css"
+  },
+  {
+    "revision": "177989b692968e92964e",
+    "url": "./static/js/2.e3778bd0.chunk.js"
+  },
+  {
+    "revision": "b6d424695dc93b12ca54",
+    "url": "./static/js/main.ca7f4aac.chunk.js"
+  },
+  {
+    "revision": "d8dc73de78900a6308fc",
+    "url": "./static/js/runtime-main.5d35b66b.js"
+  },
+  {
+    "revision": "553c69e9bcf06d9f6218730fc13fcdda",
+    "url": "./static/media/b1.553c69e9.jpg"
+  }
+]);
