b42a6e9b5d814060c2d1dbdfc1130b4520fd94b8
yk <2622811984@qq.com>
2024-05-20 17:20:38 +0800

    feat: 文案修改

--- src/routes/config.js
+++ src/routes/config.js
@@ -6,6 +6,6 @@ export default {
     menus: [
         { key: '/addChart', title: '添加图表', icon: 'mobile', component: SelectChart },
         { key: '/addBoard', title: '添加仪表盘', icon: 'mobile', component: Board },
-        { key: '/release', title: '发布链接', icon: 'mobile', component: Release },
+        { key: '/release', title: '发布链接(需先生成)', icon: 'mobile', component: Release },
     ],
 };
